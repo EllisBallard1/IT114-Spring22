1796fe99a1356cf8b0ab5ac96c5947abd4396fc5
EllisBallard <ellisballard16@gmail.com>
2022-03-02 19:21:07 -0500

    Option5, remove item from the list, success

--- app/src/main/java/com/example/stringlistlab/RemoveItemActivity.java
+++ app/src/main/java/com/example/stringlistlab/RemoveItemActivity.java
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class RemoveItemActivity extends AppCompatActivity {
+
+    @Inject
+    StringList the_list;   // reference to singleton string list object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,7 @@ public class RemoveItemActivity extends AppCompatActivity {
     {
         EditText et1;
         int pos;
+        String trash;
 
         //get the position of the item to remove
 
@@ -28,6 +38,20 @@ public class RemoveItemActivity extends AppCompatActivity {
 
         //try to delete the item from the list
 
+        try
+        {
+            the_list.remove(pos);
 
-    }
-}
+            Toast.makeText(RemoveItemActivity.this,
+                    "Item at position " + pos + " removed from the list",
+                    Toast.LENGTH_SHORT).show();
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            Toast.makeText(RemoveItemActivity.this,
+                    "Failed to remove item to the list",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    } //end of removeItem
+} //end of RemoveItemActivity
